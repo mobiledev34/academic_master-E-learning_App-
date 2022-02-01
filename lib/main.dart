d4e9571baa041ea3e68bcb7266370a84c7366db3
designerhub4 <thomas.green1218@gmail.com>
2022-02-01 16:22:09 +0530

    add admob

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:injectable/injectable.dart';
-
+// import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // MobileAds.instance.initialize();
 
   configureInjection(Environment.prod);
   await Firebase.initializeApp();
-
 
   runApp(
     AppWidget(), // Wrap your app
